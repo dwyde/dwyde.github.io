7833f38453fefbbf0b8146a8cfe394796a259ddb
David Wyde <david.wyde@gmail.com>
2020-11-15 12:52:35 -0800

    Remove old JavaScript

--- static/js/chess-games.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-SetPgnUrl("pgn4web/wyde.pgn");
-SetInitialGame(12);
-SetImagePath("pgn4web/images");
-SetAutostartAutoplay(true);
-// (head, num, chEvent, chSite, chRound, chWhite, chBlack, chResult, chDate)
-SetGameSelectorOptions("Click here to select a game...", false, 10, 0, 0, 9, 9, 7, 0);
